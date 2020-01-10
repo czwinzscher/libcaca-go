a3bf8991ec1ce81384d8cd71d0503b647ffa6051
clemens <czwinzscher2@gmail.com>
2020-01-10 12:08:28 +0100

    tests

--- cmd/libcaca-go/main.go
+++ cmd/libcaca-go/main.go
@@ -6,17 +6,24 @@ import (
 )
 
 func main() {
-	dp, err := caca.CreateDisplay(nil)
+	cv, err := caca.CreateCanvas(50, 10)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("error while creating canvas: " + err.Error())
 	}
 
-	cv := dp.GetCanvas()
-	dp.SetTitle("teitel")
-	cv.SetColorAnsi(caca.COLOR_GREEN, caca.COLOR_BLACK)
+	defer cv.Free()
+
+	cv.SetColorAnsi(caca.COLOR_GREEN, caca.COLOR_BLACK) // hacker style
 	cv.PutStr(0, 0, "hallohl")
-	dp.Refresh()
-	dp.GetEvent(caca.EVENT_KEY_PRESS, nil, -1)
+
+	dp, err := caca.CreateDisplay(&cv)
+	if err != nil {
+		log.Fatal("error while creating display: " + err.Error())
+	}
 
 	defer dp.Free()
+
+	dp.SetTitle("teitel")
+	dp.Refresh()
+	dp.GetEvent(caca.EVENT_KEY_PRESS, nil, -1)
 }
